26242e14f71172fb47d9ea151495e83dfd170206
hegifebrianto@gmail.com <hegifebrianto@gmail.com>
2023-08-21 02:42:56 +0700

    fix set server apiconfig

--- frontend/src/config/serverApiConfig.js
+++ frontend/src/config/serverApiConfig.js
@@ -1,9 +1,4 @@
-export const API_BASE_URL =
-  process.env.NODE_ENV === 'production' || process.env.REACT_APP_DEV_REMOTE === 'remote'
-    ? 'http://34.101.130.104/api/'
-    : 'https://9ffb-118-99-107-246.ngrok-free.app/api/';
-export const DOWNLOAD_BASE_URL =
-  process.env.NODE_ENV === 'production' || process.env.REACT_APP_DEV_REMOTE === 'remote'
-    ? 'http://34.101.130.104/download/'
-    : 'http://localhost:8888/download/';
+export const API_BASE_URL = 'http://34.143.146.159/api/';
+export const DOWNLOAD_BASE_URL = 'http://34.143.146.159/download';
+
 export const ACCESS_TOKEN_NAME = 'x-auth-token';
