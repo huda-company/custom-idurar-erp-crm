256a5b6f419a16d9ed6867f8221fb9b878b2062b
Hegy Febrianto <hegifebrianto@gmail.com>
2023-08-21 20:51:11 +0700

    add suplier (#21)

---
+++ frontend/src/pages/Suplier.jsx
@@ -0,0 +1,86 @@
+import React from 'react';
+
+import CrudModule from '@/modules/CrudModule';
+import CustomerForm from '@/forms/CustomerForm';
+
+function Suplier() {
+  const entity = 'client';
+  const searchConfig = {
+    displayLabels: ['company'],
+    searchFields: 'company,managerSurname,managerName',
+    outputValue: '_id',
+  };
+
+  const entityDisplayLabels = ['company'];
+
+  const readColumns = [
+    {
+      title: 'Company',
+      dataIndex: 'company',
+    },
+    {
+      title: 'Manager Surname',
+      dataIndex: 'managerSurname',
+    },
+    {
+      title: 'Manager Name',
+      dataIndex: 'managerName',
+    },
+    {
+      title: 'Email',
+      dataIndex: 'email',
+    },
+    {
+      title: 'Phone',
+      dataIndex: 'phone',
+    },
+  ];
+  const dataTableColumns = [
+    {
+      title: 'Company',
+      dataIndex: 'company',
+    },
+    {
+      title: 'Manager Surname',
+      dataIndex: 'managerSurname',
+    },
+    {
+      title: 'Manager Name',
+      dataIndex: 'managerName',
+    },
+    {
+      title: 'Email',
+      dataIndex: 'email',
+    },
+  ];
+
+  const ADD_NEW_ENTITY = 'Add New Suplier';
+  const DATATABLE_TITLE = 'Suplier List';
+  const ENTITY_NAME = 'suplier';
+  const CREATE_ENTITY = 'Create suplier';
+  const UPDATE_ENTITY = 'Update suplier';
+  const PANEL_TITLE = 'Suplier Panel';
+
+  const config = {
+    entity,
+    PANEL_TITLE,
+    ENTITY_NAME,
+    CREATE_ENTITY,
+    ADD_NEW_ENTITY,
+    UPDATE_ENTITY,
+    DATATABLE_TITLE,
+    readColumns,
+    dataTableColumns,
+    searchConfig,
+    entityDisplayLabels,
+  };
+  return (
+    <CrudModule
+      createForm={<CustomerForm />}
+      updateForm={<CustomerForm isUpdateForm={true} />}
+      config={config}
+    />
+  );
+}
+
+export default Suplier;
