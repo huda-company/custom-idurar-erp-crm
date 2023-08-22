9555f64c1c82e98ccdda2fb1f0d711f8a78db7af
huda23aduh <huda23aduh@gmail.com>
2023-08-22 23:02:41 +0700

    feat: bill n payment bill (#24)

---
+++ models/erpModels/PaymentBill.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose')
+// const AutoIncrement = require('mongoose-sequence')(mongoose)
+mongoose.Promise = global.Promise
+
+const paymentBillSchema = new mongoose.Schema({
+  removed: {
+    type: Boolean,
+    default: false
+  },
+  number: {
+    type: Number,
+    required: true
+  },
+  supplier: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'Supplier',
+    autopopulate: true,
+    required: true
+  },
+  invoice: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'Invoice',
+    required: true,
+    autopopulate: true
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+    required: true
+  },
+  amount: {
+    type: Number,
+    required: true
+  },
+  paymentMode: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'PaymentMode',
+    autopopulate: true
+  },
+  ref: {
+    type: String
+  },
+  description: {
+    type: String
+  },
+  updated: {
+    type: Date,
+    default: Date.now
+  },
+  created: {
+    type: Date,
+    default: Date.now
+  }
+})
+paymentBillSchema.plugin(require('mongoose-autopopulate'))
+module.exports = mongoose.model('PaymentBill', paymentBillSchema)
