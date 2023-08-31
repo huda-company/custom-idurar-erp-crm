e22e8e5fe3a106d9a2db62cdb3f069c5734654b2
huda23aduh <huda23aduh@gmail.com>
2023-08-31 11:15:59 +0700

    fix: download api url (#43)

--- routes/erpRoutes/erpDownloadRouter.js
+++ routes/erpRoutes/erpDownloadRouter.js
@@ -3,7 +3,7 @@ const express = require('express')
 const router = express.Router()
 
 router.route('/:directory/:filename').get(function (req, res) {
-  const file = `download/${req.params.directory}/${req.params.filename}`
+  const file = `uploads/${req.params.directory}/${req.params.filename}`
 
   res.download(file, function (err) {
     if (err) res.status(500).json({ success: false, message: "couldn't find file" }) // send a 500 on error
